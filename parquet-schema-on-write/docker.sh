762ab55be1f4e330bf1535237264af7a79150da9
oemerpeker <62536965+oemerpeker@users.noreply.github.com>
2022-04-22 14:04:49 +0200

    Update docker.sh

--- parquet-schema-on-write/docker.sh
+++ parquet-schema-on-write/docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run -p 8888:8888 -d --name jupyter jupyter/parquet-tools
+docker run -p 8888:8888 -d --name jupyter nathanhowell/parquet-tools
 docker cp main.ipynb jupyter:/home/jovyan/main.ipynb
 
